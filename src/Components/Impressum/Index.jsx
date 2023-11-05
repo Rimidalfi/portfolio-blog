05fa29718ffb636188bd0803b69153e4464d892b
Rick <rlochner@gmail.com>
2023-11-05 09:11:21 +0100

    adding impressum

---
+++ src/Components/Impressum/Index.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import { createClient } from "contentful";
+import ImpressumPost from "./ImpressumPost";
+
+const entryID = "3QmiOyhMCCGpPkcG9gxcqr";
+// Import enviromental variables deconstructed
+const { VITE_SPACE_ID, VITE_ACCESS_TOKEN } = import.meta.env;
+const Impressum = () => {
+  const [impressumpost, setImpressumpost] = useState(null);
+  useEffect(() => {
+    //erstellen Client mit Zugangsdaten
+    const client = createClient({
+      space: VITE_SPACE_ID,
+      environment: "master",
+      accessToken: VITE_ACCESS_TOKEN,
+    });
+    //API Fetch der spezifischen Daten
+    client
+      .getEntry(entryID)
+      .then((entry) => {
+        setImpressumpost(entry.fields);
+      })
+      .catch(console.error);
+  }, []);
+  console.log(
+    impressumpost !== null
+      ? ("Impressum:", impressumpost.impressumText.content)
+      : "Lade Impressum Objekt"
+  );
+  return (
+    <>
+      {impressumpost !== null ? (
+        <ImpressumPost
+          impressumJSON={impressumpost.impressumText}
+        />
+      ) : (
+        <p>LOADING ...</p>
+      )}
+    </>
+  );
+};
+
+export default Impressum;
