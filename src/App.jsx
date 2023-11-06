7ee4d5e959602d0430a5e11ed9b8271dd5ffa509
Rick <rlochner@gmail.com>
2023-11-06 07:47:20 +0100

    finish-aboutUs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import Blog from "./Components/Blog/Index";
 import Impressum from "./Components/Impressum/Index";
+import AboutUs from "./Components/AboutUs/Index";
 import MainLayout from "./Components/MainLayout";
 
 export default function App() {
   return (
     <>
       <MainLayout>
-        <Blog />
+        <AboutUs />
+        {/* <Blog /> */}
         {/* <Impressum /> */}
       </MainLayout>
     </>
