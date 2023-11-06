ec15f190f96763e3b0e84ae6433e78abff33fb02
unknown <you@example.com>
2023-11-06 14:42:34 +0100

    create navi

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
 import {useState, useEffect } from "react"
 
+import APITest from "./Components/Test/APITest";
+import MainLayout from "./Components/MainLayout";
 
 export default function App() {
 
  
   return (
     <>
-  
+      <MainLayout>
+        <APITest />
+      </MainLayout>
     </>
   );
  }
